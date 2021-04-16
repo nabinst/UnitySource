457d6c6da7fc7f214c6d6ddd3d86baded1dd33e9
nabinst <nabinst@hotmail.com>
2021-04-16 16:09:49 -0400

    changed ignore file

--- unitywebapp/settings.py
+++ unitywebapp/settings.py
@@ -33,15 +33,15 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 
-DEBUG = False
-ALLOWED_HOSTS = ['unitycenter.pythonanywhere.com']
+# DEBUG = False
+# ALLOWED_HOSTS = ['unitycenter.pythonanywhere.com']
 
 # DEBUG = False
 # ALLOWED_HOSTS = ['nabinst.pythonanywhere.com']
 
 
-# DEBUG = True
-# ALLOWED_HOSTS = []
+DEBUG = True
+ALLOWED_HOSTS = []
 
 
 
