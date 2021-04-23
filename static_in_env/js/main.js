8ca41f729970a97ba00244062e01b849674746a5
nabinst <nabinst@hotmail.com>
2021-04-23 10:06:53 -0400

    pdf filter Icon

--- static_in_env/js/main.js
+++ static_in_env/js/main.js
@@ -110,40 +110,9 @@
 		// }, 100);
 	});
 
-
-	$('nav .dropdown').click(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
-
 	$('#dropdown04').on('show.bs.dropdown', function () {
 	  console.log('show');
 	});
-
-	
-	$('.filters').on('click', 'a', function() {
-		
-		$('.filters a').removeClass('active');
-		$(this).addClass('active');
-	  });
-
-
-
 	// scroll
 	var scrollWindow = function() {
 		$(window).scroll(function(){
